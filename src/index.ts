ae08d0a1b8b7d783db7a941c676491b6a91f75f8
PeachScript <scdzwyxst@gmail.com>
2022-10-25 18:58:50 +0800

    feat: set default build config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export default 'Hello father 4!';
+import type { IApi } from 'father';
+
+export default (api: IApi) => {
+  // modify default build config for all rc projects
+  api.modifyDefaultConfig((memo) => {
+    Object.assign(memo, {
+      esm: {
+        output: 'es',
+      },
+      cjs: {
+        // specific platform to browser, father 4 build cjs for node by default
+        platform: 'browser',
+        output: 'lib',
+      },
+    } as typeof memo);
+
+    return memo;
+  });
+};
