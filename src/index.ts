e52d9842868488ab156ebcf67846e45208f3e951
afc163 <afc163@gmail.com>
2024-09-14 23:46:11 +0800

    Revert "fix: skip if eslint is not installed (#7)"
    This reverts commit b5ab939420e08440c47528b42865583e282344e2.

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,15 @@ export default (api: IApi) => {
       api?.config?.esm?.input || api?.config?.esm?.input || 'src/';
 
     const { execSync } = require('child_process');
-    try {
-      execSync(
-        `npx eslint ${inputFolder} --ext .tsx,.ts --rule '@typescript-eslint/consistent-type-exports: error'`,
-        {
-          cwd: process.cwd(),
-          env: process.env,
-          stdio: [process.stdin, process.stdout, process.stderr],
-          encoding: 'utf-8',
-        },
-      );
-    } catch (error) {
-      console.log('ESLint is not installed, skip.');
-    }
+    execSync(
+      `npx eslint ${inputFolder} --ext .tsx,.ts --rule '@typescript-eslint/consistent-type-exports: error'`,
+      {
+        cwd: process.cwd(),
+        env: process.env,
+        stdio: [process.stdin, process.stdout, process.stderr],
+        encoding: 'utf-8',
+      },
+    );
   });
 
   // modify default build config for all rc projects
